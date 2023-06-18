14811e8c3ce6be68f2819f452026f68f700e0453
Andreea Mocrii <mocriiandreea@yahoo.com>
2023-06-18 18:01:41 +0300

    remove unnecessary dependencies

--- lib/providers/patient_provider.dart
+++ lib/providers/patient_provider.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 import 'package:firebase_database/firebase_database.dart';
 
 class Patients with ChangeNotifier {
